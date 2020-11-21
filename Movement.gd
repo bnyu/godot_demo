d7c61c16098361a18e247dce8c8b8a1dcc18b9ea
bnyu <xuyue1106@gmail.com>
2020-11-21 22:56:32 +0800

    init

---
+++ Movement.gd
@@ -0,0 +1,57 @@
+extends KinematicBody2D
+#extends RigidBody2D
+
+var DEBUG_DRAW = true
+
+export var speed = 400  # Movement speed.
+var av = Vector2.ZERO  # Avoidance vector.
+var avoid_weight = 1  # How strongly to avoid other units.
+var target_radius = 50  # Stop when this close to target.
+var target = null setget set_target  # Set this to move.
+var selected = false  # Is this unit selected?
+var velocity = Vector2.ZERO
+
+#func _ready():
+	
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	if target:
+		# If there's a target, move toward it.
+		velocity = position.direction_to(target)
+		if position.distance_to(target) < target_radius:
+			target = null
+	# Find avoidance vector and add to velocity.
+	av = avoid()
+	velocity = (velocity + av * avoid_weight).normalized()
+	if velocity.length() > 0:
+		# Rotate body to point in movement direction.
+		rotation = velocity.angle()
+	#linear_velocity = velocity * speed
+	move_and_slide(velocity * speed)
+	update()
+		
+func set_target(value):
+	target = value
+
+func avoid():
+	# Calculates avoidance vector based on nearby units.
+	var result = Vector2.ZERO
+	var neighbors = $Space.get_overlapping_bodies()
+	if !neighbors:
+		return result
+	for n in neighbors:
+		result += n.position.direction_to(position)
+	result /= neighbors.size()
+	return result.normalized()
+	
+func _draw():
+	# Draws some debug vectors.
+	if !DEBUG_DRAW:
+		return
+	draw_circle(Vector2.ZERO, $Space/SpaceShape.shape.radius,
+				Color(1, 1, 0, 0.2))
+	draw_line(Vector2.ZERO, av.rotated(-rotation)*50, Color(1, 0, 0), 5)
+	draw_line(Vector2.ZERO, velocity.rotated(-rotation)*speed, Color(0, 1, 0), 5)
+	if target:
+		draw_line(Vector2.ZERO, position.direction_to(target).rotated(-rotation)*50,
+			Color(0, 0, 1), 5)
---
+++ World.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var selected = []  # Array of currently selected units.
+var drag_start = Vector2.ZERO  # Location where drag began.
+var select_rect = RectangleShape2D.new()  # Collision shape for drag box.
+		
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				for item in selected:
+					item.collider.selected = false
+				selected = []
+				drag_start = event.position
+			else:
+				update()
+				var drag_end = event.position
+				# Extents are measured from center.
+				select_rect.extents = (drag_end - drag_start) / 2
+				var space = get_world_2d().direct_space_state
+				var query = Physics2DShapeQueryParameters.new()
+				query.set_shape(select_rect)
+				query.transform = Transform2D(0, (drag_end + drag_start) / 2)
+				# Result is an array of dictionaries. Each has a "collider" key.
+				selected = space.intersect_shape(query)
+				for item in selected:
+					item.collider.selected = true
+		# If there is a selection, give it the target.
+		elif event.button_index == BUTTON_RIGHT:
+			for item in selected:
+				item.collider.target = event.position
+
+	elif event is InputEventMouseMotion:
+		# Draw the box while dragging.
+		update()
+				
+func _draw():
+	draw_rect(Rect2(drag_start, get_global_mouse_position() - drag_start),Color(.5, .5, .5), false)
