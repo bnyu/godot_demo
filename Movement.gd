133a94596115bc7042eb9fe3b36421d8d1ddedec
bnyu <xuyue1106@gmail.com>
2020-11-25 18:14:36 +0800

    move and avoid

--- Movement.gd
+++ Movement.gd
@@ -2,53 +2,80 @@ extends KinematicBody2D
 
 var DEBUG_DRAW = true
 
-export var speed = 100  # Movement speed.
-var av = Vector2.ZERO  # Avoidance vector.
-var avoid_weight = 10  # How strongly to avoid other units.
-var target_radius = 100  # Stop when this close to target.
-var target = null setget set_target  # Set this to move.
-var selected = false  # Is this unit selected?
-var velocity = Vector2.ZERO
+export var speed: float = 100  # Movement speed.
 
-func _physics_process(delta):
-	velocity = Vector2.ZERO
-	if target:
-		# If there's a target, move toward it.
-		velocity = position.direction_to(target)
+var target_radius: float = 100  # Stop when this close to target.
+
+var body_radius: float = 0
+var move_keep_radius: float = 0
+var rest_keep_radius: float = 0
+
+var to_target: bool = false
+var target: Vector2 = Vector2.ZERO setget set_target
+
+var going_velocity: Vector2 = Vector2.ZERO
+var avoid_velocity: Vector2 = Vector2.ZERO
+var velocity: Vector2 = Vector2.ZERO
+
+var no_avoid: bool = false
+
+func _ready() -> void:
+	body_radius = $Shape.shape.radius
+	move_keep_radius = $MoveSpace/Shape.shape.radius
+	rest_keep_radius = $RestSpace/Shape.shape.radius
+	target_radius = rest_keep_radius * 1.2
+
+func _physics_process(_delta) -> void:
+	if to_target:
 		if position.distance_to(target) < target_radius:
-			target = null
-	# Find avoidance vector and add to velocity.
-	av = avoid()
-	if av.length() > 0:
-		velocity = (velocity + av).normalized()
+			to_target = false
+			going_velocity = Vector2.ZERO
+		else:
+			going_velocity = get_going_velocity()
+	if no_avoid:
+		avoid_velocity = Vector2.ZERO
+	else:
+		avoid_velocity = get_avoid_velocity()
+	velocity = (going_velocity + avoid_velocity).normalized()
 	if velocity.length() > 0:
-		# Rotate body to point in movement direction.
-		rotation = velocity.angle()
 		move_and_slide(velocity * speed)
 	update()
 
+func get_going_velocity() -> Vector2:
+	return position.direction_to(target)
+
+func get_avoid_velocity() -> Vector2:
+	var result = Vector2.ZERO
+	var keep_distance: float = 0
+	var neighbors = []
+	if to_target:
+		neighbors = $MoveSpace.get_overlapping_bodies()
+		keep_distance = move_keep_radius - body_radius
+	else:
+		neighbors = $RestSpace.get_overlapping_bodies()
+		keep_distance = rest_keep_radius - body_radius
+	if keep_distance > 0:
+		for n in neighbors:
+			var distance: float = n.position.distance_to(position)
+			if distance > 0:
+				var n_body_radius = body_radius #todo
+				var factor = 1 - (distance - body_radius - n_body_radius) / keep_distance
+				if factor > 0:
+					var direction = n.position.direction_to(position)
+					result += direction * factor
+	return result
+
 func set_target(value):
 	target = value
-
-func avoid():
-	# Calculates avoidance vector based on nearby units.
-	var result = Vector2.ZERO
-	var neighbors = $Space.get_overlapping_bodies()
-	for n in neighbors:
-		var distance = n.position.distance_to(position)
-		if distance > 0:
-			var factor = 1 - (distance - 2*$Body.shape.radius) / ($Space/SpaceShape.shape.radius - $Body.shape.radius)
-			if factor > 0:
-				var direction = n.position.direction_to(position)
-				result += direction * factor
-	return result
+	to_target = true
 
 func _draw():
 	# Draws some debug vectors.
 	if !DEBUG_DRAW:
 		return
-	draw_circle(Vector2.ZERO, $Body.shape.radius,Color(1, 0, 1, 0.4))
-	#draw_circle(Vector2.ZERO, $Space/SpaceShape.shape.radius,Color(1, 1, 0, 0.2))
-	draw_line(Vector2.ZERO, av.rotated(-rotation)*speed, Color(1, 0, 0), 4)
+	draw_circle(Vector2.ZERO, body_radius, Color(1, 0, 1, 0.4))
+	draw_circle(Vector2.ZERO, move_keep_radius, Color(1, 1, 0, 0.04))
+	draw_circle(Vector2.ZERO, rest_keep_radius, Color(1, 1, 0, 0.2))
+	draw_line(Vector2.ZERO, avoid_velocity.rotated(-rotation)*speed, Color(1, 0, 0), 4)
 	draw_line(Vector2.ZERO, velocity.rotated(-rotation)*speed, Color(0, 1, 0), 2)
 
--- World.gd
+++ World.gd
@@ -4,13 +4,11 @@ var selected = []  # Array of currently selected units.
 var drag_start = Vector2.ZERO  # Location where drag began.
 var selecting = false  # start drag select.
 var select_rect = RectangleShape2D.new()  # Collision shape for drag box.
-		
+
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			if event.pressed:
-				for item in selected:
-					item.collider.selected = false
 				selected = []
 				drag_start = event.position
 				selecting = true
@@ -25,8 +23,6 @@ func _unhandled_input(event):
 				query.transform = Transform2D(0, (drag_end + drag_start) / 2)
 				# Result is an array of dictionaries. Each has a "collider" key.
 				selected = space.intersect_shape(query)
-				for item in selected:
-					item.collider.selected = true
 				update()
 		# If there is a selection, give it the target.
 		elif event.button_index == BUTTON_RIGHT:
